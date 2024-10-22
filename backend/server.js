c69342dfc5603cd48939eaa428c04e119753f981
Pratham Singh <prathambsingh891@gmail.com>
2024-10-22 20:11:39 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
 const ruleRoutes = require('./routes/ruleRoutes');
 
 const app = express();
@@ -10,7 +9,6 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public'));
 
 mongoose.connect('mongodb+srv://sanbox-user:HLbu9JqLvx8fVJmN@sandbox-cluster0.ckfzpbu.mongodb.net/?retryWrites=true&w=majority&appName=sandbox-cluster0', {
   useNewUrlParser: true,
@@ -20,8 +18,6 @@ mongoose.connect('mongodb+srv://sanbox-user:HLbu9JqLvx8fVJmN@sandbox-cluster0.ck
 }).catch(err => {
   console.error("Error connecting to MongoDB", err);
 });
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/rules', ruleRoutes);
 
